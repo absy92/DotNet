063eadaab5604378707b3a1c193a8e2283fe7abc
Abhilash Das <abhilashd@cybage.com>
2022-06-01 11:40:11 +0530

    Code update

---
+++ HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PALMCalculator
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Calculat(string firstNumber, string secondNumber, string Cal)
+        {
+            int a = Convert.ToInt32(firstNumber);
+            int b = Convert.ToInt32(secondNumber);
+            int c = 0;
+            switch (Cal)
+            {
+                case "Add":
+                    c = a + b;
+                    break;
+                case "Sub":
+                    c = a - b;
+                    break;
+                case "Mul":
+                    c = a * b;
+                    break;
+                case "Div":
+                    c = a / b;
+                    break;
+            }
+            ViewBag.Result = c;
+            return View();
+        }
+    }
+}
